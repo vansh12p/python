5a44b6e5fa089d62282ef2b9962b117c364988d2
vansh12p <girivansh26@gmail.com>
2025-07-01 01:12:31 +0530

    update more question

--- python/for_loop.py
+++ python/for_loop.py
@@ -1,4 +1,19 @@
-a,b=25,30
-print(a+b)
-print(a-b)
-print(a*b)
+# Practice question 
+#1
+"""list=[1,2,3,4]
+for num in list :
+    print(num)"""
+    
+# Question 1 : print the elements of the following list using a loop:
+"""list=[1,4,9,16,25,36,49,64,81,100]
+for num in list :
+    print(num)"""
+# Question 2 : search for a numer x in this tuple using loop:
+nums=(1,4,9,16,25,36,49,64,81,100,1)
+x=int(input("Enter an searching num : "))
+idx=0
+for val in nums :
+    if (val==x):
+        print("num is found idx " , idx)
+    idx+=1
+        
