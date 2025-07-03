a9564c751ef64f5b920b6a6d9f04862b2bd52a97
vansh12p <girivansh26@gmail.com>
2025-07-03 22:51:57 +0530

    function problem add

---
+++ python/function.py
@@ -0,0 +1,28 @@
+# Question 1 : WAF to print the length of a list. (list is the parameter)
+"""cities=["delhi","mumbai","chennai","noida","gurgaon"]
+heroes=["thor","shaktimaan","ironman","caption america"]
+def print_len(list):
+    print(len(list))
+print_len(cities)
+print_len(heroes)"""
+
+# Question 2 : WAF to print the element of a list in a single line. (list is the parameter)
+"""list=[1,2,3,4,5,6]
+def print_ele(list):
+    for item in list:
+        print(item,end=" ")
+print_ele(list)"""
+    
+# Question 3 : WAF to find the factorial of n.(n is the parameter)
+"""def fact(n):
+    factorial=1
+    for i in range(1,n+1):            
+        factorial*=i 
+    print(factorial) 
+fact(8)"""       
+    
+# Question 4 : WAF to convert USD to INR
+def usd_to_inr(usd):
+    inr=78*usd
+    print(inr)
+usd_to_inr(3)
