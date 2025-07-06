d2ef26c862d5dc763d722588f349b435367667f7
vansh12p <girivansh26@gmail.com>
2025-07-06 21:52:09 +0530

    new

--- python/string.py
+++ python/string.py
@@ -7,6 +7,34 @@ print(len("name"))"""
 print(str.count("$"))"""
 
 # Questinon 3 : remove 1 in list
-list=[2,1,3]
+"""list=[2,1,3]
 list.remove(1)
-print(list)
+print(list)"""
+
+# question 3 : Reverse a string
+"""str="hello"
+rev=str[::-1]
+print(rev)"""
+
+# Question 4 : Count vowels in a string
+"""str=input("Enter an string : ")
+count=0
+for chr in str:
+    if chr in "aeiouAEIOU":
+        count+=1
+print(count)"""
+
+# Question 5 : Check if string is palindrome
+"""str=input("Enter an string : ")
+rev=str[::-1]
+if str==rev:
+    print("palindrome")
+else:
+    print("not palindrome")"""
+    
+# Question 6 : Count words in a sentence
+sentence = input("Enter an string : ")
+word=sentence.split()
+count=len(word)
+print(count)
+    
